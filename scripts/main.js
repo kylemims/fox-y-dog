e9cbe45b67d53e681a65a081562acec176d69da5
Kyle Mims <kylemims@gmail.com>
2025-04-03 09:28:27 -0500

    start of book 5

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const foxButton = document.querySelector(fetch("https://randomfox.ca/floof/"))
+foxButton.addEventListener("click", () => {
+  
+})
+
+const dogButton = document.querySelector("#dog-button")
+dogButton.addEventListener("click", () => {
+  
+})
+
